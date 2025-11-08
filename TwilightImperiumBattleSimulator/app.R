fc763015b579d670866493512dc567c5e8dcada4
Kyle-Maher <kyle.maher@jacks.sdstate.edu>
2025-11-08 15:26:29 -0600

    Feature - App - Show output in DT tables

--- TwilightImperiumBattleSimulator/app.R
+++ TwilightImperiumBattleSimulator/app.R
@@ -2,10 +2,9 @@
 ##################
 # To Do:
 # Adjust selection so that previous selections are not available
-# Adjust Reset to go to one Options
-# Add selection option to show faction specific units
 # Add num rounds selection
 # Add filters to the wiki data
+# Overall "Draw" calculation incorrect
 ##################
 
 
@@ -63,8 +62,28 @@ ui <- navbarPage("Twilight Imperium Resources",
             # actionButton("clear_defending_units", "Clear")
           ),
           uiOutput("defending_unit_selection")
+        )
+      ),
+      fluidRow(
+        column(4,
+          DTOutput("results")
         ),
         column(4,
+          DTOutput("meta_data")
+        )
+      ),
+      fluidRow(
+        column(8,
+          DTOutput("attacker_stats")
+        )
+      ),
+      fluidRow(
+        column(8,
+          DTOutput("defender_stats")
+        )
+      ),
+      fluidRow(
+        column(8,
           verbatimTextOutput("current_selection")
         )
       )
@@ -83,10 +102,10 @@ ui <- navbarPage("Twilight Imperium Resources",
 
 
 server <- function(input, output, session) {
-  attacking_unit_count <- reactiveVal(0)
-  defending_unit_count <- reactiveVal(0)
 
-  observeEvent(input$add_attacker, {
+  # Functions
+
+  add_attacker <- function() {
     attacking_unit_count(attacking_unit_count() + 1)
     unit_choices <- if(input$show_faction_specific) all_unit_choices else base_unit_choices
 
@@ -114,9 +133,9 @@ server <- function(input, output, session) {
         )
       )
     )
-  })
+  }
 
-  observeEvent(input$add_defender, {
+  add_defender <- function() {
     defending_unit_count(defending_unit_count() + 1)
     unit_choices <- if(input$show_faction_specific) all_unit_choices else base_unit_choices
 
@@ -144,50 +163,32 @@ server <- function(input, output, session) {
         )
       )
     )
+  }
+
+  # Sever Setup
+  attacking_unit_count <- reactiveVal(0)
+  defending_unit_count <- reactiveVal(0)
+
+
+  # Action buttons to add units
+  observeEvent(input$add_attacker, {
+    add_attacker()
+  })
+  observeEvent(input$add_defender, {
+    add_defender()
   })
 
+  # Reset
   observeEvent(input$clear, {
     removeUI(selector = "div.attacker_input", multiple = TRUE)
     removeUI(selector = "div.defender_input", multiple = TRUE)
     attacking_unit_count(0)
     defending_unit_count(0)
     updateCheckboxInput(session, "show_faction_specific", value = FALSE)
+    add_attacker()
+    add_defender()
   })
 
-
-  observeEvent(input$simulate, {
-    output$current_selection <- renderPrint({
-
-      # Collect attacker units
-      attackers <- list()
-      for(i in seq_len(attacking_unit_count())){
-        unit <- input[[paste0("attacker_unit_", i)]]
-        count <- input[[paste0("attacker_counter_", i)]]
-        if(!is.null(unit) && !is.null(count)){
-          attackers[[unit]] <- count
-        }
-      }
-
-      # Collect defender units
-      defenders <- list()
-      for(i in seq_len(defending_unit_count())){
-        unit <- input[[paste0("defender_unit_", i)]]
-        count <- input[[paste0("defender_counter_", i)]]
-        if(!is.null(unit) && !is.null(count)){
-          defenders[[unit]] <- count
-        }
-      }
-
-      attacker_units_dict <- r_to_py(lapply(attackers, as.integer))
-      defender_units_dict <- r_to_py(lapply(defenders, as.integer))
-
-      # simulate_battles() is from simulate.py
-      sim <- simulate_battles(attacker_units_dict, defender_units_dict)
-
-      sim
-
-    })
-  })
 
   observeEvent(input$show_faction_specific, {
     unit_choices <- if(input$show_faction_specific) all_unit_choices else base_unit_choices
@@ -196,12 +197,11 @@ server <- function(input, output, session) {
     auc <- attacking_unit_count()
     for (i in seq_len(auc)){
       selected <- input[[paste0("attacker_unit_", i)]]
-      selection <- if (selected %in% unit_choices) selected else default_unit
       updateSelectInput(
         session,
         inputId = paste0("attacker_unit_", i),
-        choices = unit_choices,
-        selected = selection
+        choices = unique(c(unit_choices, selected)),  # Keep prev selection
+        selected = selected
       )
     }
 
@@ -209,16 +209,54 @@ server <- function(input, output, session) {
     duc <- defending_unit_count()
     for (i in seq_len(duc)){
       selected <- input[[paste0("defender_unit_", i)]]
-      selection <- if (selected %in% unit_choices) selected else default_unit
       updateSelectInput(
         session,
         inputId = paste0("defender_unit_", i),
-        choices = unit_choices,
-        selected = selection
+        choices = unique(c(unit_choices, selected)),  # Keep prev selection
+        selected = selected
       )
     }
   })
 
+
+  observeEvent(input$simulate, {
+
+    # Collect attacker units
+    attackers <- list()
+    for(i in seq_len(attacking_unit_count())){
+      unit <- input[[paste0("attacker_unit_", i)]]
+      count <- input[[paste0("attacker_counter_", i)]]
+      if(!is.null(unit) && !is.null(count)){
+        attackers[[unit]] <- count
+      }
+    }
+
+    # Collect defender units
+    defenders <- list()
+    for(i in seq_len(defending_unit_count())){
+      unit <- input[[paste0("defender_unit_", i)]]
+      count <- input[[paste0("defender_counter_", i)]]
+      if(!is.null(unit) && !is.null(count)){
+        defenders[[unit]] <- count
+      }
+    }
+
+    attacker_units_dict <- r_to_py(lapply(attackers, as.integer))
+    defender_units_dict <- r_to_py(lapply(defenders, as.integer))
+
+    # Call simulate_battles() from simulate.py
+    sim <- simulate_battles(attacker_units_dict, defender_units_dict)
+
+    output$results <- renderDT(sim[[1]], options = list(dom = "t"))
+    output$meta_data <- renderDT(sim[[2]], options = list(dom = "t"))
+    output$attacker_stats <- renderDT(sim[[3]], options = list(dom = "t"))
+    output$defender_stats <- renderDT(sim[[4]], options = list(dom = "t"))
+    output$current_selection <- renderPrint(list(attackers, defenders))
+  })
+
+
+
+  # Wiki Data Output
   output$all_units <- renderDT(df)
 
 }
